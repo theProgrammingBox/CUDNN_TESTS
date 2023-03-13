34f7a222b9c95030121ba566d925aa411e5f7c79
theProgrammingBox <justinboxgea@gmail.com>
2023-03-13 12:16:19 -0700

    not yet

--- CUDNN_TESTS/Source.cpp
+++ CUDNN_TESTS/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cuda_runtime.h>
+#include <cublas_v2.h>
 #include <cudnn.h>
 #include <curand.h>
 
@@ -35,54 +36,77 @@ int main()
 	curandCreateGenerator(&curandGenerator, CURAND_RNG_PSEUDO_DEFAULT);
 	curandSetPseudoRandomGeneratorSeed(curandGenerator, 1234ULL);
 
-	// simple matrix multiplication
-	const uint32_t INPUTS = 8;
-	const uint32_t OUTPUTS = 4;
-	const uint32_t BATCH = 1;
-
-	// create the input matrix
-	cudnnTensorDescriptor_t inputDesc;
-	cudnnCreateTensorDescriptor(&inputDesc);
-	cudnnSetTensor4dDescriptor(inputDesc, CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT, BATCH, INPUTS, 1, 1);
-	float* input;
-	cudaMalloc(&input, BATCH * INPUTS * sizeof(float));
-	curandGenerateUniform(curandGenerator, input, BATCH * INPUTS);
-
-	// create the output matrix
-	cudnnTensorDescriptor_t outputDesc;
-	cudnnCreateTensorDescriptor(&outputDesc);
-	cudnnSetTensor4dDescriptor(outputDesc, CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT, BATCH, OUTPUTS, 1, 1);
+	// simple 8 by 8 image with 3 channels, filter is 3 by 3 with 3 channels, stride is 1, padding is 0
+	const uint32_t imageRows = 8;
+	const uint32_t imageCols = 8;
+	const uint32_t imageChannels = 1;
+	const uint32_t filterRows = 3;
+	const uint32_t filterCols = 3;
+	const uint32_t filterChannels = 1;
+	const uint32_t stride = 1;
+	const uint32_t padding = 0;
+	
+	// calculate output size
+	const uint32_t outputRows = 6;
+	const uint32_t outputCols = 6;
+	const uint32_t outputChannels = filterChannels;
+	const uint32_t outputSize = outputRows * outputCols * outputChannels;
+	
+	// allocate memory for image, filter, and output
+	float* image;
+	float* filter;
 	float* output;
-	cudaMalloc(&output, BATCH * OUTPUTS * sizeof(float));
-	curandGenerateUniform(curandGenerator, output, BATCH * OUTPUTS);
+	cudaMalloc(&image, imageRows * imageCols * imageChannels * sizeof(float));
+	cudaMalloc(&filter, filterRows * filterCols * filterChannels * sizeof(float));
+	cudaMalloc(&output, outputSize * sizeof(float));
 	
-	cudnnFilterDescriptor_t weightDesc;
-	cudnnCreateFilterDescriptor(&weightDesc);
-	cudnnSetFilter4dDescriptor(weightDesc, CUDNN_DATA_FLOAT, CUDNN_TENSOR_NCHW, OUTPUTS, INPUTS, 1, 1);
-	float* weight;
-	cudaMalloc(&weight, OUTPUTS * INPUTS * sizeof(float));
-	curandGenerateUniform(curandGenerator, weight, OUTPUTS * INPUTS);
+	// initialize image and filter with random numbers
+	curandGenerateUniform(curandGenerator, image, imageRows * imageCols * imageChannels);
+	curandGenerateUniform(curandGenerator, filter, filterRows * filterCols * filterChannels);
 	
+	// create tensor descriptors
+	cudnnTensorDescriptor_t imageTensorDesc;
+	cudnnTensorDescriptor_t outputTensorDesc;
+	cudnnCreateTensorDescriptor(&imageTensorDesc);
+	cudnnCreateTensorDescriptor(&outputTensorDesc);
+	cudnnSetTensor4dDescriptor(imageTensorDesc, CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT, 1, imageChannels, imageRows, imageCols);
+	cudnnSetTensor4dDescriptor(outputTensorDesc, CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT, 1, outputChannels, outputRows, outputCols);
+	
+	// create filter descriptor
+	cudnnFilterDescriptor_t filterTensorDesc;
+	cudnnCreateFilterDescriptor(&filterTensorDesc);
+	cudnnSetFilter4dDescriptor(filterTensorDesc, CUDNN_DATA_FLOAT, CUDNN_TENSOR_NCHW, filterChannels, filterRows, filterCols, 1);
+	
+	// set alpha and beta
+	float alpha = 1.0f;
+	float beta = 0.0f;
+	
+	// create convolution descriptor
 	cudnnConvolutionDescriptor_t convDesc;
 	cudnnCreateConvolutionDescriptor(&convDesc);
-	cudnnSetConvolution2dDescriptor(convDesc, 0, 0, 1, 1, 1, 1, CUDNN_CROSS_CORRELATION, CUDNN_DATA_FLOAT);
-	
-	cudnnConvolutionFwdAlgo_t algo;
-	cudnnGetConvolutionForwardAlgorithm(cudnnHandle, inputDesc, weightDesc, convDesc, outputDesc, CUDNN_CONVOLUTION_FWD_PREFER_FASTEST, 0, &algo);
-	
+	cudnnSetConvolution2dDescriptor(convDesc, padding, padding, stride, stride, 1, 1, CUDNN_CROSS_CORRELATION, CUDNN_DATA_FLOAT);
+		
+	// calculate workspace size
 	size_t workspaceSize;
-	cudnnGetConvolutionForwardWorkspaceSize(cudnnHandle, inputDesc, weightDesc, convDesc, outputDesc, algo, &workspaceSize);
+	cudnnGetConvolutionForwardWorkspaceSize(cudnnHandle, imageTensorDesc, filterTensorDesc, convDesc, outputTensorDesc, CUDNN_CONVOLUTION_FWD_ALGO_IMPLICIT_GEMM, &workspaceSize);
+	
+	// allocate workspace
 	void* workspace;
 	cudaMalloc(&workspace, workspaceSize);
+		
+	// perform convolution
+	cudnnConvolutionForward(cudnnHandle, &alpha, imageTensorDesc, image, filterTensorDesc, filter, convDesc, CUDNN_CONVOLUTION_FWD_ALGO_IMPLICIT_GEMM, workspace, workspaceSize, &beta, outputTensorDesc, output);
 	
-	const float alpha = 1.0f;
-	const float beta = 0.0f;
-	cudnnConvolutionForward(cudnnHandle, &alpha, inputDesc, input, weightDesc, weight, convDesc, algo, workspace, workspaceSize, &beta, outputDesc, output);
+	float* imageHost = new float[imageRows * imageCols * imageChannels];
+	float* filterHost = new float[filterRows * filterCols * filterChannels];
+	float* outputHost = new float[outputSize];
+	cudaMemcpy(imageHost, image, imageRows * imageCols * imageChannels * sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(filterHost, filter, filterRows * filterCols * filterChannels * sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(outputHost, output, outputSize * sizeof(float), cudaMemcpyDeviceToHost);
 	
-	float* outputHost = new float[BATCH * OUTPUTS];
-	cudaMemcpy(outputHost, output, BATCH * OUTPUTS * sizeof(float), cudaMemcpyDeviceToHost);
-	
-	PrintMatrixf32(outputHost, BATCH, OUTPUTS, "Output");
+	PrintMatrixf32(imageHost, imageRows, imageCols * imageChannels, "Image");
+	PrintMatrixf32(filterHost, filterRows, filterCols * filterChannels, "Filter");
+	PrintMatrixf32(outputHost, outputRows, outputCols * outputChannels, "Output");
 	
 	return 0;
 }
