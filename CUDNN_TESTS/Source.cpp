aedf2940cd2b01a85edac9889d7483744789381f
theProgrammingBox <justinboxgea@gmail.com>
2023-03-20 18:32:56 -0700

    working with my params

--- CUDNN_TESTS/Source.cpp
+++ CUDNN_TESTS/Source.cpp
@@ -6,18 +6,24 @@ int main() {
 	cudnnCreate(&cudnnHandle);
 
 	const uint32_t batchSize = 1;
+	
 	const uint32_t inputChannels = 1;		// number of images stacked
-	const uint32_t inputImageRows = 4;		// height of the input image
-	const uint32_t inputImageCols = 4;		// width of the input image
+	const uint32_t inputImageRows = 16;		// height of the input image
+	const uint32_t inputImageCols = 16;		// width of the input image
+	
 	const uint32_t outputChannels = 1;
-	const uint32_t outputImageRows = 2;
-	const uint32_t outputImageCols = 2;
-	const uint32_t filterRows = 3;			// weight
-	const uint32_t filterCols = 3;
-	const uint32_t verticalStride = 1;		// how many pixels to move the filter down
-	const uint32_t horizontalStride = 1;	// how many pixels to move the filter right
+	const uint32_t outputImageRows = 4;
+	const uint32_t outputImageCols = 4;
+	
+	const uint32_t filterRows = 4;			// weight
+	const uint32_t filterCols = 4;
+	
+	const uint32_t verticalStride = 4;		// how many pixels to move the filter down
+	const uint32_t horizontalStride = 4;	// how many pixels to move the filter right
+	
 	const uint32_t verticalPadding = 0;
 	const uint32_t horizontalPadding = 0;
+	
 	const uint32_t verticalDilation = 1;	// how many pixels to skip when convolving
 	const uint32_t horizontalDilation = 1;	// how many pixels to skip when convolving
 	
@@ -97,37 +103,15 @@ int main() {
 	float* input = new float[batchSize * inputChannels * inputImageRows * inputImageCols];
 	float* output = new float[batchSize * outputChannels * outputImageRows * outputImageCols];
 	float* filter = new float[outputChannels * inputChannels * filterRows * filterCols];
-	input[0] = 0.0f;
-	input[1] = 1.0f;
-	input[2] = 0.0f;
-	input[3] = 0.0f;
-	
-	input[4] = 0.0f;
-	input[5] = 1.0f;
-	input[6] = 0.0f;
-	input[7] = 0.0f;
 
-	input[8] = 0.0f;
-	input[9] = 0.0f;
-	input[10] = 1.0f;
-	input[11] = 0.0f;
-	
-	input[12] = 0.0f;
-	input[13] = 0.0f;
-	input[14] = 0.0f;
-	input[15] = 1.0f;
-
-	filter[0] = 0.0f;
-	filter[1] = 1.0f;
-	filter[2] = 0.0f;
-	
-	filter[3] = 0.0f;
-	filter[4] = 1.0f;
-	filter[5] = 0.0f;
-	
-	filter[6] = 0.0f;
-	filter[7] = 0.0f;
-	filter[8] = 1.0f;
+	for (int i = 0; i < batchSize * inputChannels * inputImageRows * inputImageCols; i++)
+	{
+		input[i] = (float)rand() / (float)RAND_MAX;
+	}
+	for (int i = 0; i < outputChannels * inputChannels * filterRows * filterCols; i++)
+	{
+		filter[i] = (float)rand() / (float)RAND_MAX;
+	}
 
 	// allocating memory on the gpu
 	float* gpuInput;
@@ -211,4 +195,40 @@ int main() {
 		}
 		printf("\n");
 	}
+
+	// print the input and filter
+	for (uint32_t i = 0; i < batchSize; i++)
+	{
+		for (uint32_t j = 0; j < inputChannels; j++)
+		{
+			for (uint32_t k = 0; k < inputImageRows; k++)
+			{
+				for (uint32_t l = 0; l < inputImageCols; l++)
+				{
+					printf("%f ", input[i * inputChannels * inputImageRows * inputImageCols + j * inputImageRows * inputImageCols + k * inputImageCols + l]);
+				}
+				printf("\n");
+			}
+			printf("\n");
+		}
+		printf("\n");
+	}
+
+	for (uint32_t i = 0; i < outputChannels; i++)
+	{
+		for (uint32_t j = 0; j < inputChannels; j++)
+		{
+			for (uint32_t k = 0; k < filterRows; k++)
+			{
+				for (uint32_t l = 0; l < filterCols; l++)
+				{
+					printf("%f ", filter[i * inputChannels * filterRows * filterCols + j * filterRows * filterCols + k * filterCols + l]);
+				}
+				printf("\n");
+			}
+			printf("\n");
+		}
+		printf("\n");
+	}
+	printf("\n");
 }
